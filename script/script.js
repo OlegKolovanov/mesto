a72a7b380e07e00d6831cc1874b292eee61151c0
Oleg Kolovanov <olegkolovanov@yandex.ru>
2021-11-20 18:48:24 +0300

    3ya popytka

--- script/script.js
+++ script/script.js
@@ -1,32 +1,32 @@
 //open popup
-let overlayPopup = document.querySelector('.popup-overlay');
+let overlayPopup = document.querySelector('.popup');
 let openPopup = document.querySelector('.profile__edit');
-let popup = document.querySelector('.popup');
-let usernameInput = document.querySelector('.popup__username');
-let hobbyInput = document.querySelector('.popup__hobby');
+let popup = document.querySelector('.form');
+let usernameInput = document.querySelector('.form__input_profile_username');
+let hobbyInput = document.querySelector('.form__input_profile_hobby');
 let userName = document.querySelector('.profile__username');
 let hobby = document.querySelector('.profile__hobby');
-let savePopup = document.querySelector('.popup__save')
-let closePopup = document.querySelector('.close-button');
+
+let closePopup = document.querySelector('.popup__close');
 function OpennedPopup() {
-  overlayPopup.classList.add('popup-overlay_active');
+  overlayPopup.classList.add('popup_active');
   hobbyInput.value = hobby.textContent;
   usernameInput.value = userName.textContent;
   //close popup
 }
 
 function ClosedPopup() {
-  overlayPopup.classList.remove('popup-overlay_active');
+  overlayPopup.classList.remove('popup_active');
 }
 //save popup
 function formSubmitHandler (evt) {
   evt.preventDefault();
   userName.textContent = usernameInput.value;
   hobby.textContent = hobbyInput.value;
-  overlayPopup.classList.remove('popup-overlay_active');
+  ClosedPopup()
 }
 popup.addEventListener('submit', formSubmitHandler)
 openPopup.addEventListener('click', OpennedPopup)
 closePopup.addEventListener('click', ClosedPopup)
-savePopup.addEventListener('click', ClosedPopup)
+
 //я так и не понял зачем удалять некоторые переменные (savepopup и т.д.), они же нужны для логики.
