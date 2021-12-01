5430b1fbb9dc44f2da23176f4bace2363a9a558a
Oleg Kolovanov <olegkolovanov@yandex.ru>
2021-12-01 22:18:36 +0300

    mesto2

--- script/script.js
+++ script/script.js
@@ -1,19 +1,18 @@
 //open popup
-let overlayPopup = document.querySelector('.popup');
-let openEditPopup = document.querySelector('.profile__edit');
-let formSubmit= document.querySelector('.form');
-let usernameInput = document.querySelector('.form__input_profile_username');
-let hobbyInput = document.querySelector('.form__input_profile_hobby');
-let userName = document.querySelector('.profile__username');
-let hobby = document.querySelector('.profile__hobby');
-let openAddPopup = document.querySelector('.profile__add-button');
+const openEditPopup = document.querySelector('.profile__edit');
+const formProfileSubmit= document.querySelector('.form');
+const usernameInput = document.querySelector('.form__input_profile_username');
+const hobbyInput = document.querySelector('.form__input_profile_hobby');
+const userName = document.querySelector('.profile__username');
+const hobby = document.querySelector('.profile__hobby');
+const openAddPopup = document.querySelector('.profile__add-button');
 const addPopup = document.querySelector('.popup-location')
-let popupEdit = document.querySelector('.popup-edit');
+const popupEdit = document.querySelector('.popup-edit');
 const closeEditPopup = popupEdit.querySelector('.popup__close');
 const closeAddPopup = addPopup.querySelector('.popup__close');
 const nameInput = document.querySelector('.form__input_photo_name');
 const linkInput = document.querySelector('.form__input_photo_link');
-const saveAddPopup = addPopup.querySelector('.form__save');
+const formAddSubmit = addPopup.querySelector('.form');
 const picPopup = document.querySelector('.popup-picture');
 const closePicPopup = picPopup.querySelector('.popup__close');
 const elements = document.querySelector('.elements');
@@ -46,72 +45,69 @@ const initialCards = [
   }
 ]; 
 //open popup
-function opennedEditPopup() {
-  popupEdit.classList.add('popup_active');
-  hobbyInput.value = hobby.textContent;
-  usernameInput.value = userName.textContent;
-}
+//function opennedEditPopup() {
+ // popupEdit.classList.add('popup_active');
+//  hobbyInput.value = hobby.textContent;
+ // usernameInput.value = userName.textContent;
+//}
 
-function opennedAddpopup() {
-  addPopup.classList.add('popup_active');
+//function opennedAddpopup() {
+ // addPopup.classList.add('popup_active');
   
-}
+//}
 
-function openPicPopup(evt){
-  picPopup.classList.add('popup_active');
-  const namePicPopup = picPopup.querySelector('.image-container__title');
-  const imgPicPopup = picPopup.querySelector('.image-container__pic');
-  imgPicPopup.src = evt.target.src;
-  const targetElement = evt.target.closest('.element');
-  namePicPopup.textContent = targetElement.querySelector('.element__title').textContent
+//function openPicPopup(evt){
+//  picPopup.classList.add('popup_active');
+//  const namePicPopup = picPopup.querySelector('.image-container__title');
+//const imgPicPopup = picPopup.querySelector('.image-container__pic');
+ // imgPicPopup.src = evt.target.src;
+ // const targetElement = evt.target.closest('.element');
+//  namePicPopup.textContent = targetElement.querySelector('.element__title').textContent
+//}
+
+function openPopup(popup){
+  popup.classList.add('popup_active');
 }
 
 //close popup
-function closedEditPopup() {
-   popupEdit.classList.remove('popup_active');
-}
-
-function closedAddPopup() {
-  addPopup.classList.remove('popup_active');
-}
-
-function closedPicPopup() {
-  picPopup.classList.remove('popup_active');
+function closePopup(popup){
+  popup.classList.remove('popup_active');
 }
 
 //save popup
-function formSubmitHandler (evt) {
+function handleProfileSubmit (evt) {
   evt.preventDefault();
   userName.textContent = usernameInput.value;
   hobby.textContent = hobbyInput.value;
-  closedEditPopup()
+  closePopup(popupEdit);
 }
 
-function handleAdd() {
+function handleAddSubmit(evt) {
+  evt.preventDefault();
   const inputNamePhoto = nameInput.value;
   const inputLinkPhoto = linkInput.value;
   const listCard = getItem({name: inputNamePhoto, link: inputLinkPhoto});
   elements.prepend(listCard);
-
   nameInput.value = '';
   linkInput.value = '';
-  closedAddPopup()
+  closePopup(addPopup)
 }
+
 //Функция рендер: вставляет темплейты
 function render () {
-  const card = initialCards.map((item) => {
+  const cards = initialCards.map((item) => {
     return getItem(item)
   });
-  elements.append(...card);
+  elements.append(...cards);
 }
- //Функция ГетАйтем: дублирует темплейты, задаёт параметры для карточек( название, фото)
+ //Функция ГетАйтем: дублирует темплейты, задаёт параметры для карточек(название, фото)
 function getItem(item) {
   const typeItem = elementTemplate.querySelector('.element').cloneNode(true);
 
 
   const photoItem = typeItem.querySelector('.element__photo');
   photoItem.src = item.link;
-
+  photoItem.alt = item.name;
 
   const titleItem = typeItem.querySelector('.element__title');
   titleItem.textContent = item.name;
@@ -123,33 +119,52 @@ function getItem(item) {
 
 
   const trashButton = typeItem.querySelector('.element__trash')
-  trashButton.addEventListener('click', handleDelete)
+  trashButton.addEventListener('click', handlePicRemove)
  
 
   const imageElement = typeItem.querySelector('.element__photo');
-  imageElement.addEventListener('click', openPicPopup);
+  imageElement.addEventListener('click', function(evt){
+    openPopup(picPopup);
+    const namePicPopup = picPopup.querySelector('.image-container__title');
+    const imgPicPopup = picPopup.querySelector('.image-container__pic');
+    imgPicPopup.src = evt.target.src;
+    const targetElement = evt.target.closest('.element');
+    namePicPopup.textContent = targetElement.querySelector('.element__title').textContent
+  });
 
   return typeItem;
 }
+
 //Удаление фотографии
-function handleDelete(event) {
+function handlePicRemove(event) {
   const targetElement = event.target;
   const listItem = targetElement.closest('.element');
   listItem.remove();
 }
 //функция лайка
 function handleLike(event) {
-  const listItem = event.target.closest('.element');
-  const like = listItem.querySelector('.element__like-button')
-  like.classList.toggle('element__like-button_active');
+  event.target.classList.toggle('element__like-button_active');
 }
 //Слушатели
-formSubmit.addEventListener('submit', formSubmitHandler);
-openEditPopup.addEventListener('click', opennedEditPopup);
-closeEditPopup.addEventListener('click', closedEditPopup);
-openAddPopup.addEventListener('click', opennedAddpopup);
-closeAddPopup.addEventListener('click',closedAddPopup);
-closePicPopup.addEventListener('click', closedPicPopup);
-saveAddPopup.addEventListener('click', handleAdd);
+formProfileSubmit.addEventListener('submit', handleProfileSubmit);
+
+openEditPopup.addEventListener('click', function(){
+  hobbyInput.value = hobby.textContent;
+  usernameInput.value = userName.textContent;
+  openPopup(popupEdit)
+});
+closeEditPopup.addEventListener('click', function(){
+  closePopup(popupEdit)
+});
+openAddPopup.addEventListener('click', function(){
+  openPopup(addPopup)
+});
+closeAddPopup.addEventListener('click', function(){
+  closePopup(addPopup)
+});
+closePicPopup.addEventListener('click', function(){
+  closePopup(picPopup);
+});
+formAddSubmit.addEventListener('submit', handleAddSubmit);
 
 render ()
